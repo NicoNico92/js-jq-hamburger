28212fd86c9b489c229b06dfae5e1df19732beb3
Nicola92 <nicola.rizzi.92@gmail.com>
2019-11-25 22:06:55 +0100

    Caricati html, css e js

---
+++ main.js
@@ -0,0 +1,19 @@
+$('.fa-bars').click(function() {
+    $('.hamburger-menu').fadeIn();
+});
+
+$('.close').click(function() {
+    $('.hamburger-menu').fadeOut();
+});
+
+
+//VERSIONE AddClass
+
+// $(document).ready(function(){
+//     $('.header-right > a').click(function(){
+//         $('.hamburger-menu').addClass('active')
+//     })
+//     $('.hamburger-menu > a').click(function(){
+//         $('.hamburger-menu').removeClass('active')
+//     })
+// });
